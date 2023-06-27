b737b3b57c778b58cdbc53674307d8ccaa75dbab
austinchabaud <austinc1023@yahoo.com>
2023-06-26 20:04:05 -0500

    simple java intro

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+        String name = "Austin";
+
+        int[] nums = {5,4,3,2,1};
+
+        for(int n : nums){
+            System.out.println(n);
+        }
+    }
+}
