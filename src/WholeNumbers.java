c36a4e9c4713c41e72ce94c8421a817f90a431a4
austinchabaud <austinc1023@yahoo.com>
2023-06-28 15:07:59 -0500

    completed variables section

---
+++ src/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 800000000L;
+        System.out.println("The global population is " + globalPopulation);
+        long dailyGoogleSearches = 8500000000L;
+        System.out.println("Daily there are " + dailyGoogleSearches + "daily google searches");
+
+    }
+}
